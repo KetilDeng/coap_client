35a290299e6248ca22ccad25b646ed90956a4f87
Ketil <dkts47@163.com>
2019-12-30 21:23:22 +0800

    2019/12/30 commit

--- coapClient/src/main/java/com/ketil/coapClient/App.java
+++ coapClient/src/main/java/com/ketil/coapClient/App.java
@@ -1,32 +1,25 @@
 package com.ketil.coapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.CoapClient;
-import org.eclipse.californium.core.coap.MediaTypeRegistry;
-import java.net.URI;
-import java.io.IOException;
 
 public class App 
 {
     public static void main( String[] args ) {
-		URI uri = null;
-		try{
-			uri = new URI("localhost:5683");
-			CoapClient client = new CoapClient(uri);
-			try {
-				CoapResponse response = client.get();
-				// CoapResponse response = client.post("hello",0);
-				// CoapResponse response = client.put("hello",0);
-				// CoapResponse response = client.delete();
-				if(response !=null){
-					System.out.println("Code:" + response.getCode());
-					System.out.println("Options:" + response.getOptions());
-					System.out.println("Text:" + response.getResponseText());
-				}
-			} catch (Exception e) {
-				System.out.println("Exception thrown  :" + e);
-			} 
-		}catch(Exception e){
+		CoapClient client = new CoapClient("134.175.209.166:5684/api");
+		try {
+			CoapResponse response = client.get();
+			// CoapResponse response = client.post("hello",0);
+			// CoapResponse response = client.put("hello",0);
+			// CoapResponse response = client.delete();
+			if(response !=null){
+				System.out.println("Code:" + response.getCode());
+				System.out.println("Options:" + response.getOptions());
+				System.out.println("payload:" + response.getPayload());
+				System.out.println("Text:" + response.getResponseText());
+
+			}
+		} catch (Exception e) {
 			System.out.println("Exception thrown  :" + e);
-		}
+		} 
     }
 }
